d98f303bdce16ab6b1a63ea6c5c6b621fe4e64c1
toshi <toshi@toshi.click>
2022-03-16 19:42:15 +0900

    config類追加

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # next-ssg-blog
+
+
+## Directory structure
+
+- `src/`: blog articles
+  - `default/`: English articles
+  - `ja/`: Japanese articles
+- `public/`: resource files
+  - `images/`: images used in blog articles
+
+## Production build
+
+```sh
+$ npm ci --no-optional
+$ npm run download
+$ npm run build
+$ npm run export
+$ npx http-server out
+```
+
+## Debug build
+
+```sh
+$ npm i
+$ npm run dev
+```
--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = {
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  trailingSlash: true,
+  experimental: {
+    modern: true
+  },
+  webpack: (config, { dev, isServer }) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|mp4)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next",
+            name: "static/media/[name].[hash].[ext]"
+          }
+        }
+      ]
+    });
 
-module.exports = nextConfig
+    if (!dev && !isServer) {
+      // Replace React with Preact only in client production build
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat"
+      });
+    }
+
+    return config;
+  }
+};
