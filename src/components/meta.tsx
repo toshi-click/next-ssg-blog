c06fd47b3444a72c247a058e2ad7860bef1dda02
toshi <toshi@toshi.click>
2022-06-25 15:56:35 +0900

    meta修正中

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -7,29 +7,29 @@ const Meta = () => {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href="/images/icons/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href="/images/icons/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href="/images/icons/favicon-16x16.png"
       />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="manifest" href="/manifest.webmanifest" />
+      {/*<link*/}
+      {/*  rel="mask-icon"*/}
+      {/*  href="/favicon/safari-pinned-tab.svg"*/}
+      {/*  color="#000000"*/}
+      {/*/>*/}
+      <link rel="shortcut icon" href="/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="msapplication-config" content="/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
